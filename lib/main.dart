29998372878f8c79bc4ab707c24d5a6fbff8a9df
Shree1027 <shreewaghmare9022@gmail.com>
2024-10-08 23:39:39 +0530

    aspect_ratio

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  const MainApp({super.key});
+
+  @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+       appBar: AppBar(
+        title: const Text(
+          "Aspect Ratio Demo",
+          style: TextStyle(
+            fontSize: 30,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+       ),
+
+       body: Center(
+        child: Container(
+          height: 300,
+          width: 300,
+
+          color: Colors.amber,
+          child:Image.network("https://tse1.mm.bing.net/th?id=OIP.65vUdiM5kBxkLGRjJ2GtPQHaHa&pid=Api&P=0&h=180",
+          fit: BoxFit.cover,
+          
+          )
+        ),
+       ),
+      ),
+    );
+  }
+}
